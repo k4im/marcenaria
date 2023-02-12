a655366a9bfe1d5aac984549620621a838485547
João Victor <108486349+k4im@users.noreply.github.com>
2023-02-12 13:55:33 -0300

    AtualizacaoToggleMenu

--- toggle.js
+++ toggle.js
@@ -5,13 +5,30 @@ let hamburger = document.querySelector(".toggle-menu");
 
 let toggleBar = document.querySelector(".toggle-bar");
 let toggleClose = document.querySelector(".close-toggle");
+let itemLink = [...document.querySelectorAll(".item__link")];
 
-hamburger.addEventListener("click", () => {
+function openMenu() {
     toggleBar.classList.add("toggle-bar-active")
     toggleBar.classList.remove("toggle-bar");
-})
+}
 
-toggleClose.addEventListener("click", () => {
+function closeMenu() {
     toggleBar.classList.remove("toggle-bar-active")
     toggleBar.classList.add("toggle-bar");
+}
+//Evento para abrir o toggle menu
+hamburger.addEventListener("click", () => {
+    openMenu();
+});
+
+//Evento para fechar o toggle menu
+toggleClose.addEventListener("click", () => {
+    closeMenu();
+});
+
+//Evento em cada item para fechar quando clicado
+itemLink.forEach(item => {
+    item.addEventListener("click", () => {
+        closeMenu();
+    })
 })
